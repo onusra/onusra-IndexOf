93f493fa4da42520ac5e0b5208fcb066396f172f
onusra <onusra@angelo.edu>
2023-07-12 16:59:02 +0000

    Completed

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -13,6 +13,8 @@ public class IndexOf {
     public static void main(String [] args){
         
     }
+
+    
     public int getIndex(int[] arr, int n){
         for(int i = 0; i< arr.length; i++){
             if(arr[i] == n){
